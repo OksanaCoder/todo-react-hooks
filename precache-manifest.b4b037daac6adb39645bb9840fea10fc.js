9d2408ca2a1e69a0d14ea33ac139fbd5c7c90403
Oksana <myzove@mail.ru>
2020-08-18 04:37:52 +0300

    Updates

---
+++ precache-manifest.b4b037daac6adb39645bb9840fea10fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3566282e426aa84e0d7223e947df128a",
+    "url": "/todo-react-hooks/index.html"
+  },
+  {
+    "revision": "ed8ab6041e16e594dcf7",
+    "url": "/todo-react-hooks/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a5b63a54ea4d6b084050",
+    "url": "/todo-react-hooks/static/css/main.933c697f.chunk.css"
+  },
+  {
+    "revision": "ed8ab6041e16e594dcf7",
+    "url": "/todo-react-hooks/static/js/2.bae2a6b4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-react-hooks/static/js/2.bae2a6b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b63a54ea4d6b084050",
+    "url": "/todo-react-hooks/static/js/main.ba3426cc.chunk.js"
+  },
+  {
+    "revision": "bb698a35f1d00ef2864a",
+    "url": "/todo-react-hooks/static/js/runtime-main.924fdcdc.js"
+  },
+  {
+    "revision": "9b14649d374e9c7a4ab61ac731f8cbc4",
+    "url": "/todo-react-hooks/static/media/back.9b14649d.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-react-hooks/static/media/logo.5d5d9eef.svg"
+  }
+]);
